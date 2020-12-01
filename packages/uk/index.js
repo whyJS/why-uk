4b4f4de084d0a9153a89ff634591ae2faa22f1d5
why <why3935308@163.com>
2020-12-01 17:31:01 +0800

    asasa

--- packages/uk/index.js
+++ packages/uk/index.js
@@ -3,7 +3,8 @@
  * @Author: whyjs
  * @Date: 2020-09-21 13:45:11
  * @LastEditors: whyjs
- * @LastEditTime: 2020-12-01 16:17:32
+ * @LastEditTime: 2020-12-01 17:30:54
  */
+
 export const add = (a, b) => a - b
 console.log(add(1, 2))
